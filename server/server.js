8981d4ab92d34b5e4fc47c2c9744b80be0289448
anonn78103 <139428061+anonn78103@users.noreply.github.com>
2025-08-15 18:06:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,15 +18,9 @@ app.set('trust proxy', 1);
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['https://url-trimmer.01k.in', 'http://localhost:80'],
+  origin: ['https://url-trimmer.01k.in', 'https://localhost:80'],
   credentials: true
 }));
-
-app.options('*', cors({
-  origin: ['https://url-trimmer.01k.in', 'http://localhost:80'],
-  credentials: true
-}));
-
 
 app.use(express.json());
 app.use(express.static('public'));
